5472c9da34a426540fab29e7b16632fb98309b8a
quocbao <quocbao180702@gmail.com>
2023-12-16 14:06:40 +0700

    hoan thanh do an quan ly du an pham mem

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
 <!-- Remove the container if you want to extend the Footer to full width. -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/64ec3f69b2d3e13950ec765a/1h8tdfddp';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 <div >
   <!-- Footer -->
   <footer
--- index.php
+++ index.php
@@ -127,21 +127,6 @@ require_once 'thuvien.php';
 
     <!-- Similar modals for Product 2 and Product 3 -->
     <?php include_once "footer.php"; ?>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/64ec3f69b2d3e13950ec765a/1h8tdfddp';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
