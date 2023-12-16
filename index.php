ada27137c3f28702285faad68476022533bd903b
quocbao <quocbao180702@gmail.com>
2023-12-16 11:58:53 +0700

    them chat

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    require_once 'config.php';
-    require_once 'thuvien.php';
+require_once 'config.php';
+require_once 'thuvien.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -75,7 +75,7 @@
                                 echo '<div class="col-md-4">
                                     <div class="d-flex justify-content-center">
                                     <div class="product-card text-center">
-                                    <img src="'.LayHinhDauTien($value['NoiDung']) .'" class="img-fluid product-image" alt="Product 2" height="200px">
+                                    <img src="' . LayHinhDauTien($value['NoiDung']) . '" class="img-fluid product-image" alt="Product 2" height="200px">
                                     <h4>' . $value['TieuDe'] . '</h4>
                                     <p>Giá: ' . $value['Gia'] . ' VNĐ</p>
                                     <a class = "btn btn-primary" href="tours/tours_chitiet.php?id=' . $value['ID'] . '">
@@ -98,35 +98,50 @@
                     <h2 class="mb-4 text-center">Blogs Nổi Bật</h2>
                     <div class="row">
                         <?php
-                            $sqlBlogs = 'SELECT * FROM noidungtin WHERE MaBaiViet ORDER BY LuotXem DESC LIMIT 3';
-                            $cmdBlogs = $conn->prepare($sqlBlogs);
-                            $cmdBlogs->execute();
-                            $blogs = $cmdBlogs->fetchAll();
-                            foreach ($blogs as $blog) {
-                                echo '<div class="col-md-4">
+                        $sqlBlogs = 'SELECT * FROM noidungtin WHERE MaBaiViet ORDER BY LuotXem DESC LIMIT 3';
+                        $cmdBlogs = $conn->prepare($sqlBlogs);
+                        $cmdBlogs->execute();
+                        $blogs = $cmdBlogs->fetchAll();
+                        foreach ($blogs as $blog) {
+                            echo '<div class="col-md-4">
                                 <div class="d-flex justify-content-center">
                                     <div class="product-card text-center">
-                                        <img src="'.LayHinhDauTien($blog['NoiDung']) .'" class="img-fluid product-image" alt="Product 1" height: 200px;>
-                                        <h4>'. $blog['TieuDe'].'</h4>
-                                        <p>' . $blog['TomTat'].'</p>
+                                        <img src="' . LayHinhDauTien($blog['NoiDung']) . '" class="img-fluid product-image" alt="Product 1" height: 200px;>
+                                        <h4>' . $blog['TieuDe'] . '</h4>
+                                        <p>' . $blog['TomTat'] . '</p>
                                         <a class="btn btn-primary" href="tours_chitiet.php?id=' . $blog['MaBaiViet'] . '">
                                             <p class="card-text">Chi Tiết</p>
                                         </a>
                                     </div>
                                 </div>
                             </div>';
-                            }
+                        }
                         ?>
 
                     </div>
                 </section>
-              
+
             </main>
         </div>
     </div>
 
     <!-- Similar modals for Product 2 and Product 3 -->
     <?php include_once "footer.php"; ?>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/64ec3f69b2d3e13950ec765a/1h8tdfddp';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
