b261368ade1c1cc3e7e1b90fc8d4091e1df363d0
unknown <thanhdat29102002@gmail.com>
2023-12-16 11:08:48 +0700

    add footer

---
+++ footer.php
@@ -0,0 +1,101 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div >
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #929fba"
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <section class="">
+        <div class="row">
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Mô Tả</h6>
+            <p>
+              Đây là trang web để báo cáo môn lập trình quản lý của nhóm 11
+            </p>
+          
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fas fa-home mr-3"></i> Địa chỉ: Trường Đại Học An Giang</p>
+            <p><i class="fas fa-envelope mr-3"></i> Gmail: nhom11@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> SĐT:+84 3456789</p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+            <!-- Facebook -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #3b5998"
+               href="https://www.facebook.com/"
+               role="button"
+               ><i class="bi bi-facebook"></i>
+              </a>
+
+            <!-- Twitter -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #55acee"
+               href="https://www.twitter.com/"
+               role="button"
+               ><i class="bi bi-twitter"></i>
+              </a>
+
+            <!-- Google -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #dd4b39"
+               href="https://www.instagram.com/"
+               role="button"
+               ><i class="bi bi-instagram"></i>
+              </a>
+
+            <!-- Instagram -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #000000"
+               href="https://www.google.com/"
+               role="button"
+               ><i class="bi bi-google"></i>
+              </a>
+          </div>
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div
+		
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+        >
+      © 2023 Copyright:
+      <a class="text-white" >Team 11</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</div>
+<!-- End of .container -->
--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <title>Tour-Du lịch</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     <!-- <style>
     body {
         background-color: #f8f9fa;
@@ -119,19 +122,14 @@
 
                     </div>
                 </section>
-                <?php include_once "footer.php"; ?>
+              
             </main>
         </div>
     </div>
 
 
     <!-- Similar modals for Product 2 and Product 3 -->
-    <footer class="bg-dark text-light text-center py-3">
-        <div class="container">
-            <p>&copy; 2023 Your Company. All rights reserved.</p>
-            <!-- Add additional information, links, or anything else you want in the footer -->
-        </div>
-    </footer>
+    <?php include_once "footer.php"; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
